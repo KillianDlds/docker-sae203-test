6d5dba46b1afb74a00a5adc2a2f1047ea750d26d
Lorenzo De Macedo <lorenzo.demacedo@outlook.fr>
2023-05-02 15:13:58 +0200

    ajout dans un repertoire git

--- html/index.php
+++ html/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
@@ -22,21 +22,10 @@
             <li><a href="">Inscription</a></li>
         </ul>
     </nav>
-
     <main>
-        <div class="grille">
-            
-            <div class="block">
-                <iframe 
-                    width="250" 
-                    height="150" 
-                    src="https://www.youtube.com/embed/RK7UNVjGOLU" 
-                    title="YouTube video player" 
-                    frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
-                </iframe>
-            </div>
 
+        <div class="grille">
+            <div class="block"></div>
             <div class="block"></div>
             <div class="block"></div>
             <div class="block"></div>
