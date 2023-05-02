fbc4818761731e0fe5587cfb94f2375b30c26193
Lorenzo De Macedo <lorenzo.demacedo@outlook.fr>
2023-05-02 14:46:11 +0200

    ajout de nos fichier des fichiers dans git

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <nav>
+        <img src="./images/logo.svg" alt="">
+        <ul>
+            <li><a href="">Accueil</a></li>
+            <li><a href="">Favoris</a></li>
+            <li><a href="">Equipe</a></li>
+        </ul>
+        <ul>
+            <li><a href="">Connexion</a></li>
+            <li><a href="">Inscription</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <div class="grille">
+            
+            <div class="block">
+                <iframe 
+                    width="250" 
+                    height="150" 
+                    src="https://www.youtube.com/embed/RK7UNVjGOLU" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+                </iframe>
+            </div>
+
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+            <div class="block"></div>
+        </div>
+    </main>
+
+</body>
+
+</html>
